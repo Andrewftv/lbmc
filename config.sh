3affb541c216f815a52f6dfc40b8f779cf1247bc
Andrew Fateyev <andrew.ftv@telco.com>
2016-01-12 11:48:18 +0200

    Project configuration

---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+config_mak="config.mak"
+cflags="CFLAGS:="
+
+# Remove prevision config.mak
+rm -f $config_mak
+
+# Parse configuration file
+while read line
+do
+    name=$line
+	if [[ ${name:0:1} != "#" ]]; then # Ignore comments
+		if [ -n "$name" ]; then       # Ignore empty strings
+    		param=${name%=*}
+    		value=${name#*\=}
+    		echo "$param : $value"
+    		echo "$param=$value" >> $config_mak
+			cflags+="-D"
+			cflags+="$param=$value"
+			cflags+=" "
+		fi
+	fi
+done < $1
+
+echo $cflags >> $config_mak
+
